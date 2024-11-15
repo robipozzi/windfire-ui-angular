bbb53f6ca09fc4d4949032a40a4f455de29b7c7d
Roberto Pozzi <r.robipozzi@gmail.com>
2024-11-15 15:17:31 +0100

    fix Restaurant model object

--- src/app/restaurants/model/restaurant.ts
+++ src/app/restaurants/model/restaurant.ts
@@ -4,19 +4,19 @@ export class Restaurant {
   id: string | null;
   name: string | null | undefined;
   address: Address;
-  phoneLabel: string | null | undefined;
-  mobileLabel: string | null | undefined;
-  emailLabel: string | null | undefined;
-  websiteLabel: string | null | undefined;
+  phone: string | null | undefined;
+  mobile: string | null | undefined;
+  email: string | null | undefined;
+  website: string | null | undefined;
   cuisine: string | null | undefined;
   
   constructor( _id: string, 
               _name: string | null | undefined,
               _address: Address, 
-              _phoneLabel: string | null | undefined,
-              _mobileLabel: string | null | undefined,
-              _emailLabel: string | null | undefined,
-              _websiteLabel: string | null | undefined,
+              _phone: string | null | undefined,
+              _mobile: string | null | undefined,
+              _email: string | null | undefined,
+              _website: string | null | undefined,
               _cuisine: string | null | undefined, ) {
     this.id = _id;
     if (_id === '') {
@@ -24,10 +24,10 @@ export class Restaurant {
     }
     this.name = _name;
     this.address = _address;
-    this.phoneLabel = _phoneLabel;
-    this.mobileLabel = _mobileLabel;
-    this.emailLabel = _emailLabel;
-    this.websiteLabel = _websiteLabel;
+    this.phone = _phone;
+    this.mobile = _mobile;
+    this.email = _email;
+    this.website = _website;
     this.cuisine = _cuisine;
   }
 
